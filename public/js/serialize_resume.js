9df7c70d10aa68603a92c25145bb35d217ca7db8
jmoh3 <jmoh3@illinois.edu>
2020-03-01 00:14:57 -0600

    Added function to post resume to mmm api

--- public/js/serialize_resume.js
+++ public/js/serialize_resume.js
@@ -30,6 +30,29 @@ function post(serialized_data) {
     xhr.send(JSON.stringify(serialized_data));
 }
 
+// General post request that serves to `server.js`
+function post_resume(serialized_data) {
+    console.log("here");
+    var xhr = new XMLHttpRequest();
+    var url = "https://hywur8ciu1.execute-api.us-east-1.amazonaws.com/dev/";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+    xhr.onload = function(e) {
+        var response = JSON.parse(xhr.response);
+        if (response['error']) {
+            $('.callout.secondary').hide();
+            $('.callout.alert').text(response['error']).show();
+        } else {
+            $('.callout.alert').hide();
+            $('.callout.secondary').text(response['message']).show();
+        }
+        $('body, html').animate({scrollTop: 0}, 'fast');
+    }
+
+    xhr.send(JSON.stringify(serialized_data));
+}
+
 // Variable to store your files
 var resumeBase64String;
 
@@ -52,7 +75,8 @@ function send_resume(e) {
         serialized_data[form_input.name] = form_input.value;
     });
     serialized_data["resume"] = resumeBase64String;
-    post(serialized_data);
+    post_resume(serialized_data);
+    // post(serialized_data);
 }
 
 // Add events
